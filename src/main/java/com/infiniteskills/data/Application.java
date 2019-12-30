cc1e248916bdb5eb9e774f289d68e5f1c9c55206
Marcus Oliveira <marcus.oliveira@m4u.com.br>
2019-12-30 20:05:42 -0300

    Criação da Session Factory

---
+++ src/main/java/com/infiniteskills/data/Application.java
@@ -0,0 +1,12 @@
+package com.infiniteskills.data;
+
+import org.hibernate.Session;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.close();
+    }
+}
